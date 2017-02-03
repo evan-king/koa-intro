32e00d03e4c4873c9aca54c2f23599377f162f60
Evan King <evan.king@bluespurs.com>
2017-02-03 03:23:02 -0400

    hello

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ module.exports.startServer = function(config) {
     
     const app = koa();
     
+    // Handle requests with 'Hello World' response
+    app.use(function*(next) {
+        this.body = 'Hello World';
+    });
+    
     // Begin
     app.listen(config.port);
     console.log('LISTENING ON: ' + config.port);
     
-    return app;
 };
--- test/transport/http-hello.test.js
+++ test/transport/http-hello.test.js
@@ -8,14 +8,14 @@ require('mocha-generators').install();
 
 describe('Empty application', function() {
     
-    it('just always returns 404', function*() {
-        yield client.get('/').expect(404);
-        yield client.get('/test').expect(404);
+    it('just always responds hello world', function*() {
+        yield client.get('/').expect(200);
+        yield client.get('/test').expect(200);
         
-        const response = yield client.get('/hello/world').expect(404);
+        const response = yield client.get('/hello/world').expect(200);
         
         expect(response.headers['content-type']).eql('text/plain; charset=utf-8');
-        expect(response.text).eql('Not Found');
+        expect(response.text).eql('Hello World');
     });
     
 });
