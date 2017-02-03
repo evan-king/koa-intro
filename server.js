5ceaf52bd270af7b56b9e4d7195e865060e61657
Evan King <evan.king@bluespurs.com>
2017-02-03 05:29:09 -0400

    json

--- server.js
+++ server.js
@@ -16,6 +16,21 @@ module.exports.startServer = function(config) {
     // Catch errors and respect response codes from HttpErrors
     app.use(ErrorHandler.middleware({pretty: config.debug}));
     
+    // Let's always serve up JSON, in a naive manner
+    if(config.debug) app.use(function*(next) {
+        yield* next;
+        
+        // Don't do this in production, it's expensive
+        this.body = JSON.stringify(this.body, null, 2);
+    });
+    
+    app.use(function*(next) {
+        yield* next;
+        if(typeof this.body != 'object') {
+            this.body = {message: this.body};
+        }
+    });
+    
     // Handle requests with 'Hello World' response
     app.use(function*(next) {
         this.body = 'Hello World';
